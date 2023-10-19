4386d1b18369891b1e7afbbe5e9fdb4b03437d84
Alex Baracho <alex_rodrigues_11@hotmail.com>
2023-10-19 05:52:08 -0300

    manipulando_arrays

---
+++ arrays_elementos.py
@@ -0,0 +1,7 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+print(lista_musicos[2], "- índice 2")
+print(lista_musicos[9], "- índice 9")
+print(lista_musicos[14], "- índice 14")
+
+musicos = (len(lista_musicos))
+print("Esse array possui", + musicos, "elementos")
